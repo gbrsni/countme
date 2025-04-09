fc71d5ede2cf81616a0a0b5a4efa8eb1a042d49b
gbrsni <72036069+gbrsni@users.noreply.github.com>
2025-04-09 08:18:01 +0200

    Consistent spacing

--- countme.py
+++ countme.py
@@ -80,7 +80,7 @@ top_hits = pd.DataFrame(columns = ['hits'])
 for os in all_oss:
     top_hits.loc[os] = pd.DataFrame(d.groupby(["week_end", "os_variant"], observed=True)["hits"].sum()).query("week_end == week_end.max() and os_variant.str.lower().str.contains(@os)").sum()["hits"]
 
-sorted_oss =[x.capitalize() for x in top_hits.sort_values(by='hits', ascending=False).index.tolist()]
+sorted_oss = [x.capitalize() for x in top_hits.sort_values(by='hits', ascending=False).index.tolist()]
 
 for fig, oss in [
     (
